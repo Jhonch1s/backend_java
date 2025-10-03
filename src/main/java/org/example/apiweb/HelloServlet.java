9b2e29eb069e218ee2e1707ac667e02faa56c6e3
Jhonch1s <jhoncforever@gmail.com>
2025-10-02 22:38:42 -0300

    primeros cambios

--- src/main/java/org/example/apiweb/HelloServlet.java
+++ src/main/java/org/example/apiweb/HelloServlet.java
@@ -10,34 +10,7 @@ import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        try{
-
-
-
-        ServletContext contexto = getServletContext();
-
-        HttpSession sesion = request.getSession();
-
-
-        if(sesion.getAttribute("logueado") != null){
-            request.getRequestDispatcher("usuarios.jsp").forward(request, response);
-        } else {
-            request.getRequestDispatcher("login.jsp").forward(request, response);
-        }
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public void destroy() {
     }
--- src/main/webapp/registro.jsp
+++
@@ -1,1 +0,0 @@
-<p>Ingrese sus datos en el LOGIN</p>
